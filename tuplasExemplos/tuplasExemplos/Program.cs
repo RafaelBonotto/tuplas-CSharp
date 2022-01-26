44eab60486770da3dc884c13f0a2637e06ec6ac2
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-26 12:09:29 -0300

    Exemplos

--- tuplasExemplos/tuplasExemplos/Program.cs
+++ tuplasExemplos/tuplasExemplos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace tuplasExemplos
 {
@@ -6,7 +7,30 @@ namespace tuplasExemplos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var listaDoBanco = new List<string>() { "Informação-1", "Informação-2" };
+            int idBanco = 154987;
+            DateTime dataBanco = DateTime.Now;
+
+            var tuplaExemplo1 = new Tuple<int, List<string>, DateTime>(idBanco, listaDoBanco, dataBanco);
+            Console.WriteLine("Exemplo 1");
+            Console.WriteLine(tuplaExemplo1.Item1);
+            Console.WriteLine(tuplaExemplo1.Item2[0]);
+            Console.WriteLine(tuplaExemplo1.Item2[1]);            
+            Console.WriteLine(tuplaExemplo1.Item3);
+
+            (int, List<string>, DateTime) tuplaExemplo2 = (idBanco, listaDoBanco, dataBanco);
+            Console.WriteLine("Exemplo 2");
+            Console.WriteLine(tuplaExemplo2.Item1);
+            Console.WriteLine(tuplaExemplo2.Item2[0]);
+            Console.WriteLine(tuplaExemplo2.Item2[1]);            
+            Console.WriteLine(tuplaExemplo2.Item3);
+
+            var tuplaExemplo3 = Tuple.Create<int, List<string>, DateTime>(idBanco, listaDoBanco, dataBanco);
+            Console.WriteLine("Exemplo 3");
+            Console.WriteLine(tuplaExemplo3.Item1);
+            Console.WriteLine(tuplaExemplo3.Item2[0]);
+            Console.WriteLine(tuplaExemplo3.Item2[1]);          
+            Console.WriteLine(tuplaExemplo3.Item3);
         }
     }
 }
